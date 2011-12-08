4b98cf2e770ebce4fa69976a8c388985db27d076
Paweł Dawczak <pawel.dawczak@gmail.com>
2011-12-08 15:00:07 +0100

    Added feature for ensuring that a user is signed in via a confirmation email by using email spec

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,4 +27,18 @@ Ticketee::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    :address        => 'smtp.gmail.com',
+    :port           => 587,
+    :domain         => 'domain.of.sender.net',
+    :authentication => 'plain',
+    :user_name      => 'pawel.dawczak',
+    :password       => 'cob666!@#',
+    :enable_starttls_auto => true
+  }
 end
